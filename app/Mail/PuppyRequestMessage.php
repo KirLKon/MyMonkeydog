71f579287a07529fb1c401a58b392628222ad5f8
KirLKon <kkonchin@gmail.com>
2024-12-11 22:47:18 +0500

    add sending mail for puppy request v5

--- app/Mail/PuppyRequestMessage.php
+++ app/Mail/PuppyRequestMessage.php
@@ -17,8 +17,11 @@ class PuppyRequestMessage extends Mailable
     public function __construct(PuppyRequest $puppyRequest)
     {
         $this->subject = __('REQUEST_FOR_PUPPY');
-        $this->message = __('NAME') . ': ' . $puppyRequest->name ;
-        $this->message .= '<br>email: ' . $puppyRequest->email ;
+        $this->message = __('NAME') . ': ' . $puppyRequest->name;
+        $this->message .= '<br>email: ' . $puppyRequest->email;
+        if (!empty($puppyRequest->puppy)) {
+            $this->message .= __('PUPPY') . ': ' . $puppyRequest->puppy->name . ' (' . $puppyRequest->puppy->get_litter->letter . ')';
+        }
         $this->message .= '<br>---'. __('YOUR_QUESTION') .'---<br>' . $puppyRequest->message;
 
     }
