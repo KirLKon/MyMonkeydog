0d31230ee03ea58de2f1801dd4696f1a9cb549f3
KirLKon <kkonchin@gmail.com>
2024-12-21 13:34:47 +0500

    add globus v2

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -21,7 +21,7 @@
     <script type="text/javascript" src="{{ asset('assets/js/jquery.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/js/modernizr.custom.79639.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/js/jquery.ba-cond.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/mmd.js?v2') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/mmd.js?v3') }}"></script>
     <!--
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-QQB9XWRSXC"></script>
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-QQB9XWRSXC"></script>
